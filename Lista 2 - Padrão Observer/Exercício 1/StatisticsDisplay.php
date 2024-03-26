1649ce65c48844afa98df9aa7bb0fbac2a000da8
Leonardo Fiamoncini <leonardo@uoou.com.br>
2024-03-25 23:40:30 -0300

    Início do primeiro exercício do padrão Observer

---
+++ Lista 2 - Padrão Observer/Exercício 1/StatisticsDisplay.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once './DisplayElement.php';
+require_once './Observer.php';
+require_once './Subject.php';
+
+class StatisticsDisplay implements DisplayElement, Observer {
+    
+    /**
+     * float $temperature
+     */
+    protected $temperature;
+    
+    /**
+     * float $humidity
+     */
+    protected $humidity;
+
+    /**
+     * float $pressure
+     */
+    protected $pressure;
+
+    /**
+     * Subject $weatherData
+     */
+    protected $weatherData;
+
+
+    public function __construct(Subject $weatherData)
+    {
+        $this->weatherData = $weatherData;
+        $weatherData->registerObserver($this);
+    }
+
+    public function update($subject)
+    {
+        $this->temperature = $subject->getTemperature();
+        $this->humidity = $subject->getHumidity();
+        $this->pressure = $subject->getPressure();
+
+        $this->display();
+    }
+
+    public function display()
+    {
+        printf("\n Temperature: %d \n Pressure: %d \n Humidity: %d \n",
+            $this->temperature,
+            $this->pressure,
+            $this->humidity
+        );
+    }
+}
---
+++ Lista 2 - Padrão Observer/Exercício 1/WeatherData.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once './Subject.php';
+
+class WeatherData implements Subject {
+
+    /**
+     * float $temperature
+     */
+    protected $temperature;
+    
+    /**
+     * float $humidity
+     */
+    protected $humidity;
+    
+    /**
+     * float $pressure
+     */
+    protected $pressure;
+
+    /**
+     * array $observers
+     */
+    protected $observers = array();
+
+
+    public function setTemperature(float $temperature) {
+        $this->temperature = $temperature;
+        $this->measurementsChanged();
+    }
+
+    public function getTemperature() {
+        return $this->temperature;
+    }
+
+
+    public function setHumidity(float $humidity) {
+        $this->humidity = $humidity;
+        $this->measurementsChanged();
+    }
+
+    public function getHumidity() {
+        return $this->humidity;
+    }
+
+
+    public function setPressure(float $pressure) {
+        $this->pressure = $pressure;
+        $this->measurementsChanged();
+    }
+
+    public function getPressure() {
+        return $this->pressure;
+    }
+
+
+    public function registerObserver(Observer $observer)
+    {
+        array_push($this->observers, $observer);
+    }
+
+    public function removeObserver(Observer $observer)
+    {
+        foreach ($this->observers as $key => $currentObserver) {
+            if ($currentObserver === $observer) {
+                array_splice($this->observers, $key);
+                break;
+            }
+        }
+    }
+
+    public function notifyObservers()
+    {
+        foreach ($this->observers as $observer) {
+            $observer->update($this);
+        }
+    }
+    
+
+    public function measurementsChanged()
+    {
+        $this->notifyObservers();
+    }
+}
