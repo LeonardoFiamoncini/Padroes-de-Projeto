295a88e03739ab56fe860789aafcba65ddddd125
Leonardo Fiamoncini <leonardo@uoou.com.br>
2024-03-30 14:25:18 -0300

    Adição de size em Beverage

--- Lista 3 - Padrão Decorator/Exercício 2/Beverage.php
+++ Lista 3 - Padrão Decorator/Exercício 2/Beverage.php
@@ -8,11 +8,32 @@ abstract class Beverage
     protected $description = 'Unknown Beverage';
 
     /**
+     * @var string $size
+     */
+    protected $size = 'P';
+
+    /**
      * @return string $this->description
      */
     public function getDescription()
     {
         return $this->description;
+    }
+
+    /**
+     * @param string $size
+     */
+    public function setSize(string $size)
+    {
+        $this->size = $size;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSize(): string
+    {
+        return $this->size;
     }
 
     /**
